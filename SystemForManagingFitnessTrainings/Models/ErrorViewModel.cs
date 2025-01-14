b21fcb2ed8d55a396f44a1f7956232269bb91351
AnakinFranzela <v.d.yanchev@gmail.com>
2025-01-14 10:13:32 +0200

    Add project files.

---
+++ SystemForManagingFitnessTrainings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemForManagingFitnessTrainings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
