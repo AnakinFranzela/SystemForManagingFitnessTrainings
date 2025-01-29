f53da042feb0a0812a542b3964c40c0def6dff00
AnakinFranzela <v.d.yanchev@gmail.com>
2025-01-29 11:36:28 +0200

    Added enum and helpers

---
+++ SystemForManagingFitnessTrainings/Enums/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace SystemForManagingFitnessTrainings.Enums
+{
+    public enum Category
+    {
+        [Display(Name = "Кардио")]
+        Cardio,
+        [Display(Name = "Силови тренировки")]
+        PowerTraining,
+        [Display(Name = "Издръжливост")]
+        Endurance,
+        [Display(Name = "Калистеника")]
+        Calisthenics,
+        [Display(Name = "Разтягане")]
+        Stretching
+    }
+}
---
+++ SystemForManagingFitnessTrainings/Helpers/StaticData.cs
@@ -0,0 +1,8 @@
+﻿namespace SystemForManagingFitnessTrainings.Helpers
+{
+    public class StaticData
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Admin = "Admin";
+    }
+}
