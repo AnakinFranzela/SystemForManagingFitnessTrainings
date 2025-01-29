e9485d9b6d7ab86aac508f6566e76d3f230816b5
AnakinFranzela <v.d.yanchev@gmail.com>
2025-01-29 13:19:04 +0200

    Added Controllers Admin and Customer

---
+++ SystemForManagingFitnessTrainings/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SystemForManagingFitnessTrainings.Helpers;
+
+namespace SystemForManagingFitnessTrainings.Controllers
+{
+    [Authorize(Roles = StaticData.Role_Admin)]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SystemForManagingFitnessTrainings/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SystemForManagingFitnessTrainings.Helpers;
+
+namespace SystemForManagingFitnessTrainings.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [Authorize(Roles = StaticData.Role_Customer)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
