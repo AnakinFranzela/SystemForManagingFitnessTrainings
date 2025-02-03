2c40f870553db0a8bf3fe72d958a036fa18e86f0
AnakinFranzela <v.d.yanchev@gmail.com>
2025-02-03 11:18:57 +0200

    Revert "Added AddScoped in Program.cs"
    This reverts commit d419c126e9fb2d7a61b3fcdbe47ea1a5dab5a7c5.

--- SystemForManagingFitnessTrainings/Program.cs
+++ SystemForManagingFitnessTrainings/Program.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using SystemForManagingFitnessTrainings.Data;
 using SystemForManagingFitnessTrainings.Entities;
 using SystemForManagingFitnessTrainings.Seeds;
-using SystemForManagingFitnessTrainings.Repositories;
-using SystemForManagingFitnessTrainings.Repositories.IRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -12,7 +10,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddScoped<IProgressRepository, ProgressRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
